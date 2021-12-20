351c83a452dbd10faad7f0ef7453173b77d0f019
Dhiraj <jdhiraj543@gmail.com>
2021-12-20 16:09:46 +0530

    created models

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to db"))
+  .catch((err) => console.log(err));
+
+
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.listen(process.env.port || 5000, () => {
+  console.log("server running");
+});
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+router.get("/usertest", (req, res) => {
+  res.send("user test");
+});
+
+router.post("/userposttest", (req, res) => {
+  const username = req.body.username;
+  console.log(username)
+});
+module.exports = router;
