47329c53f6e0fea436a6dbdc41477f043d70db15
Dhiraj <jdhiraj543@gmail.com>
2021-12-22 16:42:55 +0530

    updated to source -nc

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+// setting env file
 dotenv.config();
+const dotenv = require("dotenv");
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to db"))
   .catch((err) => console.log(err));
 
+
+app.use(cors())
 app.use(express.json());
 // always plura
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 app.listen(process.env.port || 5000, () => {
   console.log("server running");
 });
