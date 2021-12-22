f7bfa4895a5b485570dc6d53e57a8ff4716f9378
Dhiraj <jdhiraj543@gmail.com>
2021-12-22 16:32:31 +0530

    in usd, inr (recm)

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,16 +1,18 @@
 const router = require("express").Router;
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY;
+const stripe = require("stripe")(KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
     {
       source: req.body.tokenId,
       amount: req.body.amount,
-      curreny: "inr",
+      // later change ti INR
+      curreny: "usd",
     },
-    (stripeError, stripeRes) => {
-      if (stripeError) {
-        res.status(500).json(stripeError);
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
       } else {
         res.status(200).json(stripeRes);
       }
