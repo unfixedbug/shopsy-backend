c0bb8c3f22a6606c1979744fc6089eb8e47dcf91
Dhiraj <jdhiraj543@gmail.com>
2021-12-22 16:11:50 +0530

    models altered

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({ 
-    title: { type: String, required: true ,unique:true },
-    desc: { type: String, required: true},
+const ProductSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true },
     img: { type: String, required: true },
-    category: { type: Array, },
-    size: { type: String},
-    color: { type: String},
+    categories: { type: Array },
+    size: { type: String },
+    color: { type: String },
     price: { type: String, required: true },
- },{timestamps:true});
-
+    inStock: { type: boolean, default: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", ProductSchema);
