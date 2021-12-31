879494ceb166904b73b2728f059aa44395b07cb8
Dhiraj <jdhiraj543@gmail.com>
2021-12-31 13:25:04 +0530

    changes verify token

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,17 +1,17 @@
+
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
-    // take the 2nd element
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("token is not valid");
+      if (err) res.status(403).json("Token is not valid!");
       req.user = user;
       next();
     });
   } else {
-    return res.status(401).json("not authenticated");
+    return res.status(401).json("You are not authenticated!");
   }
 };
 
